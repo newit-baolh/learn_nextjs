fb1eff3e59a628ab8477f8e65dd311a721a29b89
newit-baolh <baolh@newit.co.jp>
2022-04-01 11:49:42 +0700

    dynamic render client and server

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
 
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+// import dynamic if want just render client side do not render from server side => set srr= false
+// else normal it will render from server using import normal 
+// import Header from 'components/common/header'
+const Header = dynamic(()=> import('components/common/header'), {ssr: false})
 export interface AboutPageProps {
 }
 
@@ -7,6 +13,7 @@ export default function AboutPage (props: AboutPageProps) {
   return (
     <div>
       <h1>About Page</h1>
+      <Header />
     </div>
   );
 }
